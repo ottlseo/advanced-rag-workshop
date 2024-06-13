474eca1c5fff3047a94073a6ecfce3d43a82f3e5
ottlseo <dotsi@ewhain.net>
2024-06-14 01:25:31 +0900

    Feat: Add s3 stack

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,21 +2,22 @@
 import 'source-map-support/register';
 import { App, Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
 import { EC2Stack } from "../lib/ec2Stack";
+import { S3Stack } from '../lib/s3Stack';
 
 const DEFAULT_REGION = "us-west-2";
-
-const app = new App();
-
-// Stage 생성
-const ec2Stage = new Stage(app, 'EC2Stage', {
+const envSetting = {
   env: {
     account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
     region: DEFAULT_REGION,
   },
-});
+};
 
-// EC2Stack 생성 및 Stage에 추가
-new EC2Stack(ec2Stage, 'EC2Stack');
+const app = new App();
+
+const s3Stack = new S3Stack(app, 'S3Stack', envSetting);
+
+const ec2Stack = new EC2Stack(app, 'EC2Stack', envSetting);
+ec2Stack.addDependency(s3Stack); 
 
 // TODO: 기타 스택 차례로 추가
 
--- lib/s3Stack.ts
+++ lib/s3Stack.ts
@@ -1,9 +1,27 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_s3 as s3, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 
 export class S3Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const accountId = Stack.of(this).account;
+
+    // S3 버킷 생성
+    const bucketName = `adv-rag-custom-docs-bucket-${accountId}`;
+    const bucket = new s3.Bucket(this, 'CustomBucket', {
+      bucketName: bucketName,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+    });
+
+    new CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+      description: 'S3 bucket name for custom file upload',
+    });
   }
 }
