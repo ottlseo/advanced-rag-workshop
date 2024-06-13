7392765dff920026772d32d38f644cdf04f6c1a0
ottlseo <dotsi@ewhain.net>
2024-06-13 21:27:29 +0900

    Chore: move app.ts to bin directory

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { App, Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
+import { EC2Stack } from "../lib/ec2Stack";
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const DEFAULT_REGION = "us-west-2";
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new App();
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// Stage 생성
+const ec2Stage = new Stage(app, 'EC2Stage', {
+  env: {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: DEFAULT_REGION,
+  },
 });
+
+// EC2Stack 생성 및 Stage에 추가
+new EC2Stack(ec2Stage, 'EC2Stack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: DEFAULT_REGION,
+  },
+});
+
+// TODO: 기타 스택 차례로 추가
+
+app.synth();
