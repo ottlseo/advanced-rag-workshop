00e3dd1f5c9e0f6e80a36f0a4ac63b23e52bb31e
seungwon2 <dandelionsw2@ewhain.net>
2024-06-14 09:41:58 +0900

    [Add] : add opensearch, sagemaker logic

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import { App, Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
+import "source-map-support/register";
+import { App, Stack, StackProps, Stage, StageProps } from "aws-cdk-lib";
 import { EC2Stack } from "../lib/ec2Stack";
-import { S3Stack } from '../lib/s3Stack';
+import { S3Stack } from "../lib/s3Stack";
+import { OpensearchStack } from "../lib/openSearchStack";
+import { SagemakerStack, HuggingFaceLlmProps } from "../lib/sagemakerStack";
 
 const DEFAULT_REGION = "us-west-2";
 const envSetting = {
@@ -14,11 +16,22 @@ const envSetting = {
 
 const app = new App();
 
-const s3Stack = new S3Stack(app, 'S3Stack', envSetting);
+const s3Stack = new S3Stack(app, "S3Stack", envSetting);
 
-const ec2Stack = new EC2Stack(app, 'EC2Stack', envSetting);
-ec2Stack.addDependency(s3Stack); 
+const ec2Stack = new EC2Stack(app, "EC2Stack", envSetting);
+ec2Stack.addDependency(s3Stack);
 
+const opensearchStack = new OpensearchStack(app, "OpensearchStack", envSetting);
 // TODO: 기타 스택 차례로 추가
+
+const HFprops: HuggingFaceLlmProps = {
+  name: "reranker",
+  instanceType: "ml.g5.xlarge",
+  environmentVariables: {
+    HF_MODEL_ID: "Dongjin-kr/ko-reranker",
+    HF_TASK: "text-classification",
+  },
+};
+const sagemakerStack = new SagemakerStack(app, "SagemakerStack", HFprops);
 
 app.synth();
