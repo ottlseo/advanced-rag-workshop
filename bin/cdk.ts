eb062546bc8854cc8edae97eec47b8fbc67f26a7
ottlseo <dotsi@ewhain.net>
2024-07-16 01:59:03 +0900

    Chore: add prefix to stack name

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -7,6 +7,7 @@ import { CustomResourceStack } from "../lib/customResourceStack";
 import { SagemakerNotebookStack } from "../lib/sagemakerNotebookStack/sagemakerNotebookStack";
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
+const STACK_PREFIX = "AdvancedRAG";
 const DEFAULT_REGION = "us-west-2";
 const envSetting = {
   env: {
@@ -18,22 +19,22 @@ const envSetting = {
 const app = new App();
 
 // Deploy Sagemaker stack
-const sagemakerNotebookStack = new SagemakerNotebookStack(app, "SagemakerNotebookStack", envSetting);
+const sagemakerNotebookStack = new SagemakerNotebookStack(app, `${STACK_PREFIX}-SagemakerNotebookStack`, envSetting);
 
 // Deploy OpenSearch stack
-const opensearchStack = new OpensearchStack(app, "OpensearchStack", envSetting);
+const opensearchStack = new OpensearchStack(app, `${STACK_PREFIX}-OpensearchStack`, envSetting);
 opensearchStack.addDependency(sagemakerNotebookStack);
 
-const customResourceStack = new CustomResourceStack(app, "CustomResourceStack", envSetting)
-customResourceStack.addDependency(opensearchStack)
+const customResourceStack = new CustomResourceStack(app, `${STACK_PREFIX}-CustomResourceStack`, envSetting)
+customResourceStack.addDependency(opensearchStack);
 
 // Deploy Reranker stack using cloudformation template 
-const rerankerStack = new CfnInclude(opensearchStack, 'RerankerStack', {
+const rerankerStack = new CfnInclude(opensearchStack, `${STACK_PREFIX}-RerankerStack`, {
   templateFile: 'lib/rerankerStack/RerankerStack.template.json'
 });
 
 // Deploy EC2 stack
-const ec2Stack = new EC2Stack(app, "EC2Stack", envSetting);
+const ec2Stack = new EC2Stack(app, `${STACK_PREFIX}-EC2Stack`, envSetting);
 //ec2Stack.addDependency(opensearchStack);
 ec2Stack.node.addDependency(rerankerStack);
 
